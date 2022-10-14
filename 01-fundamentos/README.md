6740e3324b6f669dc32df165484ef03df80357a7
Victoria Sales <contatovictoriasales@gmail.com>
2022-10-13 21:56:32 -0300

    Adicao dos exercicios da secao 01 bloco 01

---
+++ 01-fundamentos/README.md
@@ -0,0 +1,4 @@
+## Exercícios resolvidos das seguintes seções:
+01 - Unix & Bash
+02 - Git & GitHub
+03 - Introdução a HTML e CSS
---
+++ 01-fundamentos/secao-01-unix-e-bash/bloco-01-unix-e-bash-exercises.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#Bloco 1 - Unix e Bash Exercícios
+01
+mkdir unix_tests
+cd unix_testes
+
+02
+touch trybe.txt
+
+03
+cp trybe.txt trybe_backup.txt
+
+04
+mv trybe.txt trybe-renomeado.txt
+
+05
+mkdir backup
+
+06
+mv trybe_backup.txt backup
+
+07
+mkdir backup2
+
+08
+mv backup/trybe_backup.txt backup2
+
+09
+rmdir backup
+
+10
+mv backup2 backup
+
+11
+pwd
+ls -l .
+
+12
+rm -rd backup
+
+13
+cat >> skills.txt
+Internet
+Unix
+Bash
+HTML
+CSS
+JavaScript
+React
+SQL
+
+14
+head -n 5 skills.txt
+
+15
+tail -n 4 skills.txt
+
+16
+rm *.txt
