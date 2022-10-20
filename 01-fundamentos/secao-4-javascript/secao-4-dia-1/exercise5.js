0c0a7f33043c8f0d0dd65f49d53216b3600b00ff
Victoria Sales <contatovictoriasales@gmail.com>
2022-10-20 19:29:05 -0300

    Atualizacao de exercicios

---
+++ 01-fundamentos/secao-4-javascript/secao-4-dia-1/exercise5.js
@@ -0,0 +1,10 @@
+/* 
+Seção 4. Dia 1. Exercícios - Switch e Case
+01 - Crie uma variável para armazenar o status da pessoa candidata no processo seletivo, que pode ser: 'aprovada', 'lista' ou 'reprovada';
+02 - Crie uma estrutura condicional com o switch/case que irá imprimir as seguintes mensagens:
+03 - Caso 'aprovada', imprima “Parabéns, você foi aprovada(o)!”.
+04 - Caso 'lista', imprima “Você está na nossa lista de espera”.
+05 - Caso 'reprovada', imprima “Você foi reprovada(o)”.
+06 - Caso default, imprima a mensagem de “Informação incorreta”.
+*/
+
