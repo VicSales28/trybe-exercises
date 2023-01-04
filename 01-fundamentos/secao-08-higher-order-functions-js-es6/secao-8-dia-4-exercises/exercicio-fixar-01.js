fad5bc4d027b07dfa274125fd8dbb37ac6a0ba25
Victoria Sales <contatovictoriasales@gmail.com>
2023-01-04 08:13:06 -0300

    Atualizacao de exercicios

--- 01-fundamentos/secao-08-higher-order-functions-js-es6/secao-8-dia-4-exercises/exercicio-fixar-01.js
+++ 01-fundamentos/secao-08-higher-order-functions-js-es6/secao-8-dia-4-exercises/exercicio-fixar-01.js
@@ -1,2 +1,17 @@
-const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
-console.log(Math.max(...randomNumbers)); 
+/* 
+01 - Vamos fazer uma salada de frutas com os itens adicionais que você desejar.
+Faça uma função chamada fruitSalad, passando como parâmetro specialFruit e additionalItens; faça a função retornar uma lista única, contendo todos os itens da nossa salada de frutas, usando o spread.
+*/
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Manga', 'Melancia', 'Melão'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Aveia', 'Leite Condensado', 'Castanha de cajú'];
+
+const fruitSalad = (fruit, additional) => {
+  // Escreva sua função aqui
+  return finalResult = [...fruit,...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
